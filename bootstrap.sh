5da3c69c3bf8dc3fa89f46f6feb733796962ef77
TAKAHASHI, Kyohei <kcrt@kcrt.net>
2023-10-22 04:45:18 +0000

    remove extra single quote

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 mkdir ~/.vim
 mkdir ~/.vim/bundle
-git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim'
+git clone --depth=1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
