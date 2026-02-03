a73e82eb20a9b522d151782a1bff32b39d812c7b
TAKAHASHI, Kyohei <kcrt@kcrt.net>
2026-02-03 23:08:59 +0900

    :hammer: Refactor script name and Wine aliases
    - Rename makedir_today.sh to mkdirtoday.sh for brevity
    - Comment out game-porting-toolkit alias in favor of simpler wine binary
    - Simplify .exe handler to use wine directly

--- zsh/.zshrc.d/704-aliases-platform.zsh
+++ zsh/.zshrc.d/704-aliases-platform.zsh
@@ -17,6 +17,6 @@ alias azure_neon_stop='az vm stop --resource-group neon_group --name neon; az vm
 export WINEPREFIX="$HOME/.wine"
 if [[ "$OSTYPE" = *darwin* ]]; then
 	abbrev-alias wine_steam='wine64 ~/.wine/drive_c/Program\ Files\ \(x86\)/Steam/Steam.exe -no-cef-sandbox'
-	abbrev-alias wine_gameportingkit='LANG=ja_JP.UTF-8 LC_ALL=ja_JP.UTF-8 MTL_HUD_ENABLED=1 WINEESYNC=1 `arch -x86_64 brew --prefix game-porting-toolkit`/bin/wine64'
-	alias -s exe='wine_gameportingkit'
+	# abbrev-alias wine_gameportingkit='LANG=ja_JP.UTF-8 LC_ALL=ja_JP.UTF-8 MTL_HUD_ENABLED=1 WINEESYNC=1 `arch -x86_64 brew --prefix game-porting-toolkit`/bin/wine64'
+	alias -s exe='wine'
 fi
